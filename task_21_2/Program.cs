9fed316c4bf6fb7c4708d5a8095d49bb0a66ab2d
Sofya Kovaleva <78658225+sonyakovaa@users.noreply.github.com>
2022-02-28 21:43:13 +0400

    Update Program.cs

--- task_21_2/Program.cs
+++ task_21_2/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,29 +6,81 @@ using System.Threading.Tasks;
 
 namespace task_21
 {
-    class Program
+    public class BinaryTree
     {
-        static void Main(string[] args)
+        public class Node // вложенный класс для узлов
         {
-            BinaryTree tree = new BinaryTree();
-            using (StreamReader file_input = new StreamReader("C:/Users/sonya/source/repos/task_21/input.txt"))
+            public object inf;
+            public Node left;
+            public Node right;
+            public int depth;
+
+            public Node(object nodeInf, int depth)
             {
-                string[] text = file_input.ReadLine().Split(' ');
+                inf = nodeInf;
+                left = null;
+                right = null;
+                this.depth = depth;
+            }
 
-                for (int i = 0; i < text.Length; i++)
+            public static void Add(ref Node r, object nodeInf, int depth)
+            {
+                if (r == null) // если корень и есть место для узла, то добавляем
                 {
-                    tree.Add(int.Parse(text[i]));
+                    r = new Node(nodeInf, depth);
                 }
-                tree.Preorder();
-                Console.WriteLine();
-
-                for (int i = 1; i < text.Length; i++)
+                else
                 {
-                    int res = 0;
-                    tree.DepthBinaryTree(int.Parse(text[0]), int.Parse(text[i]), ref res);
-                    Console.WriteLine("{0} - {1}", text[i], res);
+                    if (((IComparable)(r.inf)).CompareTo(nodeInf) > 0) // nodeInf < r.inf?,
+                                                                       // возвращает 1, если r.inf следует в порядке возрастания за nodeInf
+                    {
+                        Add(ref r.left, nodeInf, depth + 1); // если да, то производим поиск места в левом
+                    }
+                    else
+                    {
+                        Add(ref r.right, nodeInf, depth + 1); // иначе в правом
+                    }
                 }
             }
+
+            public static void Preorder(Node r) // прямой обход дерева - корень, левое и правое поддерево
+            {
+                if (r != null)
+                {
+                    Console.WriteLine("{0}, depth - {1}", r.inf, r.depth);
+                    Preorder(r.left);
+                    Preorder(r.right);
+                }
+            }
+            
+        }
+
+        Node tree;
+
+        public object Inf
+        {
+            set { tree.inf = value; }
+            get { return tree.inf; }
+        }
+
+        public BinaryTree() // открытый конструктор
+        {
+            tree = null;
+        }
+
+        private BinaryTree(Node r) // закрытый конструктор
+        {
+            tree = r;
+        }
+
+        public void Add(object nodeInf)
+        {
+            Node.Add(ref tree, nodeInf, 0);
+        }
+
+        public void Preorder()
+        {
+            Node.Preorder(tree);
         }
     }
 }
