038689c7ce842871a3a39f29b8a8c2bfa8c4923c
Sofya Kovaleva <78658225+sonyakovaa@users.noreply.github.com>
2022-03-20 22:58:28 +0400

    Create Program.cs

---
+++ task_21_3/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_21_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            BinaryTree treeDelete = new BinaryTree();
+            using (StreamReader file_input = new StreamReader("C:/Users/sonya/source/repos/task_21/input.txt"))
+            {
+                string[] text = file_input.ReadLine().Split(' ');
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    tree.Add(int.Parse(text[i]));
+                }
+                tree.Preorder();
+                tree.f();
+                Console.WriteLine();
+            }
+        }
+    }
+}
