880e8f348268e4d5105a7483b4813252b541da1b
Sofya Kovaleva <78658225+sonyakovaa@users.noreply.github.com>
2022-05-17 22:35:30 +0400

    Add files via upload

---
+++ task_22_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_1
+{
+    struct SPoint //описание структуры
+    {
+        public int x, y; //поля структуры
+        public SPoint(int x, int y) //конструктор структуры
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(@"C:\Users\sonya\source\repos\graph_1\document.txt");
+            Console.WriteLine("Graph:");
+            g.Show();
+
+            int sizeGraph = g.graphSize();
+            Console.Write("Enter A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter towns: ");
+            string line = Console.ReadLine();
+            List<String> towns = line.Split().ToList();
+
+            g.Dijkstr(a, b, towns);
+
+        }
+    }
+}
